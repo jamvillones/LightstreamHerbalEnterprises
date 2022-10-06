9685ef3c53e32bc577887f2261c67362caf3bdb8
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-06 08:50:36 +0800

    changes

---
+++ EFCorePractice/EFCore.Practice.DataAccess/DBContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using EFCore.Practice.DataAccess.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Practice.DataAccess
+{
+    public class DBContextFactory : IDesignTimeDbContextFactory<POSDbContext>
+    {
+        public POSDbContext CreateDbContext(string[] args = null)
+        {
+            var option = new DbContextOptionsBuilder<POSDbContext>();
+            option.UseSqlServer("Server=DESKTOP-EP1IGTA\\SQLEXPRESS;Database=EFCOREPracticeDB;Trusted_Connection=True;");
+
+            return new POSDbContext(option.Options);
+        }
+    }
+}
---
+++ EFCorePractice/EFCore.Practice.Domain/Models/DomainObject.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCore.Practice.Domain.Models
+{
+    public class DomainObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

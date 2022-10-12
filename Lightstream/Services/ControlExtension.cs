8a34c5ddd43674abbd00371c0a0235c49b3fe024
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-12 11:58:45 +0800

    added handler for unsafe thread calls

---
+++ Lightstream/Services/ControlExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightstream.Extensions
+{
+    public static class ControlExtension
+    {
+        public static void InvokeIfRequired(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+                control.Invoke(action);
+            else
+                action.Invoke();
+
+        }
+    }
+}

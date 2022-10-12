39bfa843fc4486ea85cec3d680c9563ade4e1612
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-12 19:13:25 +0800

    changes

--- Lightstream/Forms/IngredientsForm.cs
+++ Lightstream/Forms/IngredientsForm.cs
@@ -15,7 +15,7 @@ namespace Lightstream.Forms
     {
         private DbContextFactory factory = new DbContextFactory();
         public string IngredientName => ingredientField.IngredientName;
-        public Unit? UnitOfMeasurement => ingredientField.UnitOfMeasurement;
+        public Unit SelectedUnit => ingredientField.UnitOfMeasurement;
 
         string UOMString => ingredientField.UnitOfMeasurementString;
         public decimal Cost => ingredientField.Cost;
@@ -43,7 +43,7 @@ namespace Lightstream.Forms
                 {
                     Name = this.IngredientName,
                     Cost = this.Cost,
-                    UnitMeasurement = UnitOfMeasurement is null ? new Unit() { Name = UOMString } : context.Units.FirstOrDefault(x => x.Id == UnitOfMeasurement.Id)
+                    UnitMeasurement = SelectedUnit is null ? new Unit() { Name = UOMString } : context.Units.FirstOrDefault(x => x.Id == SelectedUnit.Id)
                 };
 
                 /// add to the list of ingredients

68f558ec88f5a7d5e3689068d918cee7c8962380
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-18 10:29:03 +0800

    added extension method opening modal form

--- Lightstream/Services/ControlExtension.cs
+++ Lightstream/Services/ControlExtension.cs
@@ -14,7 +14,19 @@ namespace Lightstream.Extensions
                 control.Invoke(action);
             else
                 action.Invoke();
-
         }
+
+        public static DialogResult OpenFormModal(this Form control, FormClosedEventHandler closingCallback = null)
+        {
+            using (control)
+            {
+                if (closingCallback != null)
+                    control.FormClosed += closingCallback;
+
+                return control.ShowDialog();
+            }
+        }
+
+
     }
 }

ae557b4a304829c0363193a284b24bed705a67e2
wretchqueen <115688162+wretchqueen@users.noreply.github.com>
2022-10-21 21:20:57 +0800

    changes

--- Lightstream/Forms/LoginForm.Designer.cs
+++ Lightstream/Forms/LoginForm.Designer.cs
@@ -57,10 +57,9 @@
             this.loginBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.loginBtn.Location = new System.Drawing.Point(113, 305);
-            this.loginBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.loginBtn.Location = new System.Drawing.Point(79, 183);
             this.loginBtn.Name = "loginBtn";
-            this.loginBtn.Size = new System.Drawing.Size(399, 50);
+            this.loginBtn.Size = new System.Drawing.Size(279, 30);
             this.loginBtn.TabIndex = 1;
             this.loginBtn.TabStop = false;
             this.loginBtn.Text = "Login";
@@ -73,10 +72,9 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox2.Location = new System.Drawing.Point(0, 0);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
             this.textBox2.PlaceholderText = "username...";
-            this.textBox2.Size = new System.Drawing.Size(333, 24);
+            this.textBox2.Size = new System.Drawing.Size(233, 16);
             this.textBox2.TabIndex = 3;
             this.textBox2.TextChanged += new System.EventHandler(this.textFields_TextChanged);
             // 
@@ -85,20 +83,18 @@
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(163, 158);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(114, 95);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(333, 33);
+            this.panel1.Size = new System.Drawing.Size(233, 20);
             this.panel1.TabIndex = 4;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 31);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Location = new System.Drawing.Point(0, 19);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(333, 2);
+            this.panel2.Size = new System.Drawing.Size(233, 1);
             this.panel2.TabIndex = 5;
             // 
             // panel3
@@ -106,10 +102,9 @@
             this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel3.Controls.Add(this.textBox3);
             this.panel3.Controls.Add(this.panel4);
-            this.panel3.Location = new System.Drawing.Point(163, 210);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Location = new System.Drawing.Point(114, 126);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(333, 33);
+            this.panel3.Size = new System.Drawing.Size(233, 20);
             this.panel3.TabIndex = 5;
             // 
             // textBox3
@@ -118,11 +113,10 @@
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox3.Location = new System.Drawing.Point(0, 0);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '●';
             this.textBox3.PlaceholderText = "password...";
-            this.textBox3.Size = new System.Drawing.Size(333, 24);
+            this.textBox3.Size = new System.Drawing.Size(233, 16);
             this.textBox3.TabIndex = 3;
             this.textBox3.TextChanged += new System.EventHandler(this.textFields_TextChanged);
             // 
@@ -130,10 +124,9 @@
             // 
             this.panel4.BackColor = System.Drawing.Color.Black;
             this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel4.Location = new System.Drawing.Point(0, 31);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel4.Location = new System.Drawing.Point(0, 19);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(333, 2);
+            this.panel4.Size = new System.Drawing.Size(233, 1);
             this.panel4.TabIndex = 5;
             // 
             // label1
@@ -142,10 +135,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("The Bold Font", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(120)))), ((int)(((byte)(37)))));
-            this.label1.Location = new System.Drawing.Point(181, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(127, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(275, 47);
+            this.label1.Size = new System.Drawing.Size(182, 31);
             this.label1.TabIndex = 6;
             this.label1.Text = "Lightstream\r\n";
             // 
@@ -158,10 +150,9 @@
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox1.Image = global::Lightstream.Properties.Resources.Female_Profile_15px;
-            this.pictureBox1.Location = new System.Drawing.Point(127, 160);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(89, 96);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(19, 19);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -170,10 +161,9 @@
             // 
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox2.Image = global::Lightstream.Properties.Resources.keepass_15px;
-            this.pictureBox2.Location = new System.Drawing.Point(127, 212);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Location = new System.Drawing.Point(89, 127);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox2.Size = new System.Drawing.Size(19, 19);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -181,9 +171,9 @@
             // LoginForm
             // 
             this.AcceptButton = this.loginBtn;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 435);
+            this.ClientSize = new System.Drawing.Size(436, 261);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -192,7 +182,6 @@
             this.Controls.Add(this.loginBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";

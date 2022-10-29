824fc8712310ee183e65d4fc8fc24dd2d649ce74
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-29 22:05:12 +0800

    Create IIDModel.cs
    interface for model that contain Id

---
+++ Lightstream.DataAccess/Models/IIDModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightstream.DataAccess.Models
+{
+    public interface IIDModel
+    {
+        int Id { get; set; }
+    }
+}

aa019e5e588d8c61c7212af0c80e594f7c5e5611
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-10-29 22:06:00 +0800

    Create IGetRepository.cs
    repo for getting/reading

---
+++ Lightstream.DataAccess/Repositories/IGetRepository.cs
@@ -0,0 +1,19 @@
+﻿using Lightstream.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightstream.DataAccess.Repositories
+{
+    public interface IGetRepository<TModel> where TModel : class, IIDModel
+    {
+        Task<IEnumerable<TModel>> GetAll_Async();
+        Task<IEnumerable<TModel>> GetFiltered_Async(
+              Func<TModel, bool> filter,
+              Range? range = default);
+        Task<TModel?> Get_Async(int id);
+    }
+}

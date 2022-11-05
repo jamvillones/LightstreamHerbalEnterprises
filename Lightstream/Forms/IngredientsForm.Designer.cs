1e585b1ddf909ed4d6d422986903fbd9b05fc335
heyqti <ailynpanganiban000@gmail.com>
2022-11-05 17:17:07 +0800

    changes combobox so that the user could not type anymore she/he will only choose over the option on the list inside the combo box

--- Lightstream/Forms/IngredientsForm.Designer.cs
+++ Lightstream/Forms/IngredientsForm.Designer.cs
@@ -45,9 +45,10 @@
             this.addBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.addBtn.Font = new System.Drawing.Font("Bebas Neue", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.addBtn.Location = new System.Drawing.Point(272, 169);
+            this.addBtn.Location = new System.Drawing.Point(389, 282);
+            this.addBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.addBtn.Name = "addBtn";
-            this.addBtn.Size = new System.Drawing.Size(150, 30);
+            this.addBtn.Size = new System.Drawing.Size(214, 50);
             this.addBtn.TabIndex = 6;
             this.addBtn.Text = "SAVE";
             this.addBtn.UseVisualStyleBackColor = false;
@@ -57,9 +58,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bebas Neue", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(15, 116);
+            this.label3.Location = new System.Drawing.Point(21, 193);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.Size = new System.Drawing.Size(140, 23);
             this.label3.TabIndex = 26;
             this.label3.Text = "Unit of Measurement:";
             // 
@@ -67,9 +69,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bebas Neue", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(78, 71);
+            this.label1.Location = new System.Drawing.Point(111, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.Size = new System.Drawing.Size(40, 23);
             this.label1.TabIndex = 25;
             this.label1.Text = "Cost:";
             // 
@@ -77,9 +80,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bebas Neue", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(33, 26);
+            this.label2.Location = new System.Drawing.Point(47, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.Size = new System.Drawing.Size(112, 23);
             this.label2.TabIndex = 24;
             this.label2.Text = "Ingredient name:";
             // 
@@ -87,13 +91,14 @@
             // 
             this.unitOption.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.unitOption.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.unitOption.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.unitOption.DisplayMember = "Name";
+            this.unitOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.unitOption.FormattingEnabled = true;
-            this.unitOption.Location = new System.Drawing.Point(111, 112);
+            this.unitOption.Location = new System.Drawing.Point(159, 187);
+            this.unitOption.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.unitOption.Name = "unitOption";
-            this.unitOption.Size = new System.Drawing.Size(311, 23);
+            this.unitOption.Size = new System.Drawing.Size(427, 33);
             this.unitOption.TabIndex = 22;
             // 
             // cost
@@ -101,14 +106,15 @@
             this.cost.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cost.DecimalPlaces = 2;
-            this.cost.Location = new System.Drawing.Point(111, 67);
+            this.cost.Location = new System.Drawing.Point(159, 112);
+            this.cost.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cost.Maximum = new decimal(new int[] {
             10000000,
             0,
             0,
             0});
             this.cost.Name = "cost";
-            this.cost.Size = new System.Drawing.Size(311, 23);
+            this.cost.Size = new System.Drawing.Size(444, 31);
             this.cost.TabIndex = 23;
             this.cost.ThousandsSeparator = true;
             // 
@@ -118,17 +124,18 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.nameTxt.BackColor = System.Drawing.Color.White;
             this.nameTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.nameTxt.Location = new System.Drawing.Point(111, 22);
+            this.nameTxt.Location = new System.Drawing.Point(159, 37);
+            this.nameTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.nameTxt.Name = "nameTxt";
-            this.nameTxt.Size = new System.Drawing.Size(311, 23);
+            this.nameTxt.Size = new System.Drawing.Size(443, 31);
             this.nameTxt.TabIndex = 21;
             // 
             // IngredientsForm
             // 
             this.AcceptButton = this.addBtn;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 211);
+            this.ClientSize = new System.Drawing.Size(620, 352);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
@@ -138,6 +145,7 @@
             this.Controls.Add(this.addBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "IngredientsForm";

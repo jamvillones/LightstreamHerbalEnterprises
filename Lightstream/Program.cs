eaf9d30f35ced28a0f3a2eaa159c6d4fc644b453
WoodsPentagon <50472091+jamvillones@users.noreply.github.com>
2022-11-16 15:36:06 +0800

    Update Program.cs
    made the app single instance only

--- Lightstream/Program.cs
+++ Lightstream/Program.cs
@@ -1,4 +1,6 @@
 using Lightstream.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Lightstream
 
@@ -11,9 +13,40 @@ namespace Lightstream
         [STAThread]
         static void Main()
         {
+            var process = PriorProcess();
+            if(process is not null)
+            {
+                var windowHandle = process.MainWindowHandle;
+                ShowWindow(windowHandle, 9);
+                ShowWindow(windowHandle, 5);
+                SetForegroundWindow(windowHandle);
+
+                //window.BringToFront();
+                return;
+            }
+
             ApplicationConfiguration.Initialize();
 
             Application.Run(new Shell());
         }
+
+        public static Process PriorProcess()
+        {
+            Process curr = Process.GetCurrentProcess();
+            Process[] procs = Process.GetProcessesByName(curr.ProcessName);
+            foreach (Process p in procs)
+            {
+                if ((p.Id != curr.Id) &&
+                    (p.MainModule.FileName == curr.MainModule.FileName))
+                    return p;
+            }
+            return null;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
     }
 }
